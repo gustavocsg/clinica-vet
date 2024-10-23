383b8d930f9e288118178baea4d2cc894603c9c9
gustavocsg <gustavo.sena96@gmail.com>
2024-10-23 10:48:12 -0300

    Adionando todo o projeto no repositório.

---
+++ js/main-script.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+  function removerClasseAtivo() {
+    $(".lista-temas li a").removeClass("active");
+  }
+
+  function removerFundo() {
+    $("#topo").removeClass("bg-dark");
+    $("#topo").removeClass("bg-light");
+    $("#topo").removeClass("bg-auto");
+
+    $("body").removeClass("bg-dark");
+    $("body").removeClass("bg-light");
+    $("body").removeClass("bg-auto");
+  }
+
+  $(".lista-temas li").on("click", function () {
+    let linkElement = $(this).find("a");
+    let theme = linkElement.data("theme");
+    let iconClass = linkElement.data("icon");
+    let colorTheme = "bg-" + theme;
+
+    $("body").attr("data-bs-theme", theme);
+    $("#themeIcon").removeClass().addClass("bi").addClass(iconClass);
+    removerClasseAtivo();
+    removerFundo();
+    linkElement.addClass("active");
+    $("#topo").addClass(colorTheme);
+    $("body").addClass(colorTheme);
+  });
+});
+
+// $(document).ready(function () {
+// });
+// document.getElementById("btnSwitch").addEventListener("click", () => {
+//   if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+//     document.documentElement.setAttribute("data-bs-theme", "light");
+//   } else {
+//     document.documentElement.setAttribute("data-bs-theme", "dark");
+//   }
+// });
+
+// document.getElementById("btnSwitch").addEventListener("click", () => {
+//   if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+//     document.documentElement.setAttribute("data-bs-theme", "light");
+//   } else {
+//     document.documentElement.setAttribute("data-bs-theme", "dark");
+//   }
+// });
+
+// $(function () {
+//   $("btnSwitch").change(function () {
+//     if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+//       document.documentElement.setAttribute("data-bs-theme", "light");
+//     } else {
+//       document.documentElement.setAttribute("data-bs-theme", "dark");
+//     }
+//   });
+// });
+
+// document.getElementById("btnSwitch").change(function () {
+//   if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
+//     document.documentElement.setAttribute("data-bs-theme", "light");
+//   } else {
+//     document.documentElement.setAttribute("data-bs-theme", "dark");
+//   }
+// });
+
+// document.getElementsByClassName("lista-temas").addEventListener("click", () => {
+//   let link = this.find("a");
+//   let tema = link.data("theme");
+
+//   document.getElementsByTagName("html");
+// });
+
+// document.querySelector('.themes-list li').on('click', function(){
+//     let linkElemnt = document.querySelector(this).find('a');
+//     let theme = linkElemnt.data('theme');
+//     document.querySelector('html').attr('data-bs-theme', theme);
+// });
